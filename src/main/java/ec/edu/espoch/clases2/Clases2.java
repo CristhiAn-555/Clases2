aee9901906222d273fa6869ad928ae83127f26b0
CristhiAn-555 <cristhianvalencia999@gmail.com>
2025-04-02 10:31:40 -0500

    subiendo la  segunda clase

---
+++ src/main/java/ec/edu/espoch/clases2/Clases2.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package ec.edu.espoch.clases2;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Clases2 {
+
+    public static void main(String[] args) {
+        // datos de entrada
+        /*int opcion = 4;
+        
+        float a =0;
+        float b=6;
+        float c=4;
+        float resultado=0;
+        
+        switch (opcion ) {
+            case 1:
+                resultado = a+b;
+                System.out.println("El resultado:"+ resultado);
+                break;
+                
+            case 2:
+                resultado = a * b;
+                System.out.println("El resultado:"+resultado);
+                
+                break;
+                
+                case 3:
+                resultado = a - b;
+                System.out.println("El resultado:"+ resultado);
+                break;
+                
+                case 4:
+                
+                if(b !=0)
+                {
+                  resultado = a/b;
+                 System.out.println("el resultado es: "+resultado);   
+                }
+                else{
+                   System.out.println("error");
+                }
+                break;
+                
+                case 5:
+                   resultado = (a);
+                   System.out.println("El resultado:"+ resultado);
+                    
+                
+            default:
+                throw new AssertionError();
+        }
+ */
+        
+        
+      /*  int opcion = 2;
+        
+        
+        
+        switch (opcion ) {
+            case 1:
+                
+                System.out.println("camion ingresa alas 8  a 10 am" );
+                break;
+                
+            case 2:
+                
+                System.out.println("Moto ingresa alas 11 a 2 pm" );
+                
+                break;
+                
+                case 3:
+                
+                System.out.println("camion ingresa alas 3 a 6 pm ");
+                break;
+                
+                default:
+                throw new AssertionError();
+        
+        
+        
+
+        //PROCESO
+       /* if (edad >= 0 && edad <= 120) {
+
+            if (edad > 0 && edad < 8) {
+
+                System.out.println("Eres un niño, busca tu mamá");
+            } else if (edad > 9 && edad < 17) {
+                System.out.println("Eres un adolecentes, no tienes permiso de salir ");
+            }
+
+        }
+        if (edad > 18 && edad < 35) {
+            System.out.println("Eres un joven, busca trabajo o estudia");
+
+        } else if (edad > 36 && edad < 64) {
+            System.out.println("Eres un adulto, estas en tu mejor estapa de tu vida");
+        } else {
+            System.out.println("mensaje errraneo");
+        }
+        /* if (edad < 17 ){
+            System.out.println("Eres menor de edad ");
+
+        }
+    }*/
+      //FOR
+     /* int suma = 0;
+        for (int i = 1; i < 10; i++) {
+            suma += i;
+            System.out.println(suma+"+"+i+"="+(suma+i));
+            
+        }
+        System.out.println(suma);
+*/
+     
+     int i=0;
+     int suma = 0;
+        while (i<=20) {  
+            suma += i;
+            System.out.println(suma+" + "+ i +"="+(suma+i));
+            i++;
+            
+        }
+         System.out.println(suma);
+      
+      
+    }
+}
